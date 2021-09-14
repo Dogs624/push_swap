64669f1f435f9293d02fa41a23595b1275379596
jvander- <jvander-@student.s19.be>
2021-09-14 16:46:56 +0200

    delete .DS

--- sources/push_swap.c
+++ sources/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jvander- <jvander-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 14:00:59 by jvander-          #+#    #+#             */
-/*   Updated: 2021/09/14 16:28:38 by jvander-         ###   ########.fr       */
+/*   Updated: 2021/09/14 16:45:49 by jvander-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,19 @@
 
 int	main(int argc, char **argv)
 {
-	t_stack	*head;
+	t_stack	*stack_a;
+	t_stack *stack_b;
 
 	if (error_handling(argc, argv))
 	{
 		ft_putstr("Error\n");
 		exit(1);
 	}
-	head = ft_create_stack(argc, argv);
-	ft_display_stack(head);
-	ft_free_stack(head);
+	stack_a = ft_create_stack(argc, argv);
+	if (!stack_a)
+		exit(1);
+	ft_display_stack(stack_a);
+	ft_free_stack(stack_a);
 	check_leaks();
 	return (0);
 }
